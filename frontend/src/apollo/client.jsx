045f327d167219e13a6809fc5227fc566702e9ac
Vipul <vipulmaske2939@gmail.com>
2026-02-07 15:47:45 +0530

    Fix Apollo client and build frontend

--- frontend/src/apollo/client.jsx
+++ frontend/src/apollo/client.jsx
@@ -2,7 +2,7 @@ import { ApolloClient, InMemoryCache, createHttpLink } from "@apollo/client";
 import { setContext } from "@apollo/client/link/context";
 
 const httpLink = createHttpLink({
-  uri: "http://localhost:5000/graphql",
+  uri: import.meta.env.VITE_API_URI,
 });
 
 const authLink = setContext((_, { headers }) => {
