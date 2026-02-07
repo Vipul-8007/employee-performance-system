27d914a811e541570489c0a20803050733d16307
Vipul <vipulmaske2939@gmail.com>
2026-02-07 16:26:25 +0530

    Fix 405 by proxying GraphQL via Static Web App

--- frontend/src/apollo/client.jsx
+++ frontend/src/apollo/client.jsx
@@ -7,7 +7,6 @@ const httpLink = createHttpLink({
 
 const authLink = setContext((_, { headers }) => {
   const token = localStorage.getItem("token");
-
   return {
     headers: {
       ...headers,
